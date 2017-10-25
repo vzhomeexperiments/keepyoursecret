d64ce5045f918eb8025b8d40d3970571ba25b0f3
vzhomeexperiments <vz.home.experiments@gmail.com>
2017-10-25 22:21:13 +0200

    created scripts for auto encryption/decryption

--- Encrypt.R
+++ Encrypt.R
@@ -7,21 +7,22 @@ library(tidyverse)
 # You can encrypt both using private or public key
 # NB: Make sure you have valid copy of Private Key matching your Public Key
 
-## WHAT YOU WANT TO ENCRYPT???
-secret <- "Pass-Word-1234"
+pathPrvKeyUSB <- file.path("D:/private.key")
+pathPubKeyUSB <- file.path("D:/public.key")
 
 ## Encrypting the secret and storing that to file "password.Encrypted"
-if(identical(read_pubkey("public.key")$fingerprint,
-             read_key("private.key", password = "simple")$pubkey$fingerprint)) {
-  secret %>% 
+if(identical(read_pubkey(pathPubKeyUSB)$fingerprint,
+             read_key(pathPrvKeyUSB, password = "simple")$pubkey$fingerprint)) {
+  read_csv(file.path("C:/00_RSA", "passwords.csv")) %>% 
     # serialize the object
     serialize(connection = NULL) %>% 
     # encrypt the object
-    encrypt_envelope("public.key") %>% 
+    encrypt_envelope(pathPubKeyUSB) %>% 
     # write encrypted data to File
-    write_rds("password.Encrypted") } else { print("You are attempting to encrypt without having proper copy of Private Key")}
+    write_rds(file.path("C:/00_RSA", "password.Encrypted")) } else { print("You are attempting to encrypt without having proper copy of Private Key")}
 
 ## remove secret
 remove(list = ls())
 
-## clean console and history...
+## clean non encrypted file if encrypted file does exist and history...
+if(file.exists(file.path("C:/00_RSA", "password.Encrypted"))) {file.remove(file.path("C:/00_RSA", "passwords.csv"))}
--- keys.R
+++ keys.R
@@ -3,11 +3,14 @@ library(openssl)
 library(tidyverse)
 
 #### KEY MANAGEMENT ####
+# save that to USB Key (Windows)
+pathPrvKeyUSB <- file.path("D:/private.key")
+pathPubKeyUSB <- file.path("D:/public.key")
 # generate your private key (NB: make sure to do back up copy!!!)
 rsa_keygen(bits = 2099) %>% 
-  write_pem(path = "private.key", password = "simple")
+  write_pem(path = pathPrvKeyUSB, password = "simple")
 # generate your public key (NB: optional. Use Private Key to encrypt/decrypt)
-read_key(file = "private.key", password = "simple") %>% 
+read_key(file = pathPrvKeyUSB, password = "simple") %>% 
   # extract element of the list and write to file
-  `[[`("pubkey") %>% write_pem("public.key")
+  `[[`("pubkey") %>% write_pem(pathPubKeyUSB)
 
