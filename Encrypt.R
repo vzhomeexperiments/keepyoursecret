595619690cc7201a3cfe6bfa5bc91585d6206674
vzhomeexperiments <vz.home.experiments@gmail.com>
2017-12-03 19:01:28 +0100

    updated title

--- Encrypt.R
+++ Encrypt.R
@@ -1,3 +1,8 @@
+# ONLY FOR TRAINING PURPOSES
+# UDEMY Course: "Cryptography is more fun with R!"
+# (C) 2017 Vladimir Zhbanko, vz.home.experiments@gmail.com
+# Enjoying the code? Join the course https://udemy.com/keep-secret-under-control
+
 # Used Libraries:
 library(openssl)
 library(tidyverse)
